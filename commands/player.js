89647b4017a157ccef650a445d993bf9e0481ea1
JackShepee <salamakhaviktor@gmail.com>
2023-03-12 16:36:20 +0200

    changed search for player Nickname or ID

--- commands/player.js
+++ commands/player.js
@@ -4,64 +4,77 @@ const axios = require("axios");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("player")
-    .setDescription("Find player stats by his nickname!")
+    .setDescription("Find player stats by his nickname or ID!")
     .addStringOption((option) =>
       option
         .setName("name")
-        .setDescription("Enter player nickname!")
+        .setDescription("Enter player nickname or ID!")
         .setRequired(true)
     ),
   async execute(interaction) {
-    const playerName = interaction.options.getString("name");
+    const playerNameOrId = interaction.options.getString("name");
     try {
-      const response = await axios.get(
-        `https://api.opendota.com/api/search?q=${playerName}`
-      );
-      const playerIds = response.data;
-      if (playerIds.length > 0) {
-        const playerId = playerIds[0].account_id;
-        const responsePlayer = await axios.get(
+      // Check whether the argument is a number (i.e. player ID)
+      const isPlayerId = !isNaN(playerNameOrId);
+      
+      // Search for the player based on the argument
+      const searchUrl = isPlayerId
+        ? `https://api.opendota.com/api/players/${playerNameOrId}`
+        : `https://api.opendota.com/api/search?q=${playerNameOrId}`;
+
+      const searchResponse = await axios.get(searchUrl);
+      
+      // Get the player ID from the response (either directly or from the first search result)
+      const playerId = isPlayerId ? playerNameOrId : searchResponse.data[0]?.account_id;
+      
+      if (playerId) {
+        const playerResponse = await axios.get(
           `https://api.opendota.com/api/players/${playerId}`
         );
-        const responseStats = await axios.get(
+        const statsResponse = await axios.get(
           `https://api.opendota.com/api/players/${playerId}/wl`
         );
-        const responseRecentMatches = await axios.get(
+        const recentMatchesResponse = await axios.get(
           `https://api.opendota.com/api/players/${playerId}/recentMatches`
         );
-        const player = responsePlayer.data;
-        const playerStats = responseStats.data;
-        const playerRecentMatches = responseRecentMatches.data;
+
+        const player = playerResponse.data;
+        const stats = statsResponse.data;
+        const recentMatches = recentMatchesResponse.data;
+
+        // Use the player name from the response if available, otherwise use the argument
+        const playerName = player.personaname || playerNameOrId;
+
         const embed = new EmbedBuilder()
           .setColor("d90429")
-          .setTitle(player.profile.personaname)
+          .setTitle(player.profile.personaname || playerName)
           .setURL(player.profile.profileurl)
           .setDescription(`https://www.opendota.com/players/${player.profile.account_id}`)
           .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/c/ce/Dota-2-small-icon.png')
           .addFields(
             {
               name: "Player stats",
-              value: `Wins: ${playerStats.win}, Loses: ${
-                playerStats.lose
+              value: `Wins: ${stats.win}, Loses: ${
+                stats.lose
               }, Winrate: ${Math.round(
-                (playerStats.win / (playerStats.win + playerStats.lose)) * 100
+                (stats.win / (stats.win + stats.lose)) * 100
               )}%`,
             },
             {
               name: "Recent match:",
-              value: `https://www.opendota.com/matches/${playerRecentMatches[0].match_id}`,
+              value: `https://www.opendota.com/matches/${recentMatches[0].match_id}`,
             }
           )
           .setImage(player.profile.avatarfull);
 
         return await interaction.reply({ embeds: [embed] });
       } else {
-        return await interaction.reply(`Игрок с именем "${playerName}" не найден`);
+        return await interaction.reply(`Player "${playerNameOrId}" not found`);
       }
     } catch (error) {
       console.error(error);
       return await interaction.reply(
-        "An error occured while fetching player data!"
+        "An error occurred while fetching player data!"
       );
     }
   },
--- commands/playerheroes.js
+++ commands/playerheroes.js
@@ -8,68 +8,76 @@ module.exports = {
     .addStringOption((option) =>
       option
         .setName("name")
-        .setDescription("Enter player nickname!")
+        .setDescription("Enter player nickname or ID!")
         .setRequired(true)
     ),
   async execute(interaction) {
-    const playerName = interaction.options.getString("name");
+    const playerNameOrId = interaction.options.getString("name");
+    let playerId;
     try {
-      const response = await axios.get(
-        `https://api.opendota.com/api/search?q=${playerName}`
-      );
-      const playerIds = response.data;
-      if (playerIds.length > 0) {
-        const playerId = playerIds[0].account_id;
-        const responsePlayer = await axios.get(
-          `https://api.opendota.com/api/players/${playerId}`
-        );
-        const playerHeroesResponse = await axios.get(
-          `https://api.opendota.com/api/players/${playerId}/heroes`
-        );
-        const playerHeroes = playerHeroesResponse.data.slice(0, 5);
-
-        const heroStatsResponse = await axios.get(
-          "https://api.opendota.com/api/heroStats"
-        );
-        const heroData = heroStatsResponse.data.reduce(
-          (obj, hero) => ({ ...obj, [hero.id]: hero }),
-          {}
-        );
-
-        const embedData = playerHeroes.map((hero) => ({
-          name: heroData[hero.hero_id].localized_name,
-          value: `Games: ${hero.games}\nWins: ${hero.win}\nWinrate: ${(
-            (hero.win / hero.games) *
-            100
-          ).toFixed(2)}%`,
-          icon: `https://api.opendota.com${heroData[hero.hero_id].img}`,
-        }));
-
-        const embed = new EmbedBuilder()
-          .setColor("#0099ff")
-          .setTitle(`Top 5 heroes for ${playerName}`)
-          .setURL(
-            `https://www.opendota.com/players/${responsePlayer.data.profile.account_id}`
-          )
-          .addFields(
-            embedData.map((data) => ({
-              icon: data.icon,
-              name: data.name,
-              value: data.value,
-              inline: true
-            }))
-          )
-          .setImage(responsePlayer.data.profile.avatarfull)
-          .setThumbnail(
-            "https://upload.wikimedia.org/wikipedia/commons/c/ce/Dota-2-small-icon.png"
-          );
-
-        await interaction.reply({ embeds: [embed] });
+      // check if input is an ID
+      if (!isNaN(playerNameOrId)) {
+        playerId = playerNameOrId;
       } else {
-        return await interaction.reply(
-          `Игрок с именем "${playerName}" не найден`
+        // input is a nickname, search for player ID
+        const response = await axios.get(
+          `https://api.opendota.com/api/search?q=${playerNameOrId}`
         );
+        const playerIds = response.data;
+        if (playerIds.length > 0) {
+          playerId = playerIds[0].account_id;
+        } else {
+          return await interaction.reply(
+            `Player with name or ID "${playerNameOrId}" not found!`
+          );
+        }
       }
+
+      const responsePlayer = await axios.get(
+        `https://api.opendota.com/api/players/${playerId}`
+      );
+      const playerHeroesResponse = await axios.get(
+        `https://api.opendota.com/api/players/${playerId}/heroes`
+      );
+      const playerHeroes = playerHeroesResponse.data.slice(0, 5);
+
+      const heroStatsResponse = await axios.get(
+        "https://api.opendota.com/api/heroStats"
+      );
+      const heroData = heroStatsResponse.data.reduce(
+        (obj, hero) => ({ ...obj, [hero.id]: hero }),
+        {}
+      );
+
+      const embedData = playerHeroes.map((hero) => ({
+        name: heroData[hero.hero_id].localized_name,
+        value: `Games: ${hero.games}\nWins: ${hero.win}\nWinrate: ${(
+          (hero.win / hero.games) *
+          100
+        ).toFixed(2)}%`,
+        icon: `https://api.opendota.com${heroData[hero.hero_id].img}`,
+      }));
+
+      const embed = new EmbedBuilder()
+        .setColor("#0099ff")
+        .setTitle(`Top 5 heroes for player ${responsePlayer.data.profile.personaname}`)
+        .setURL(
+          `https://www.opendota.com/players/${responsePlayer.data.profile.account_id}`
+        )
+        .addFields(
+          embedData.map((data) => ({
+            icon: data.icon,
+            name: data.name,
+            value: data.value,
+            inline: true
+          }))
+        )
+        .setImage(responsePlayer.data.profile.avatarfull)
+        .setThumbnail(
+          "https://upload.wikimedia.org/wikipedia/commons/c/ce/Dota-2-small-icon.png"
+        );
+
+      await interaction.reply({ embeds: [embed] });
     } catch (error) {
       console.error(error);
       return await interaction.reply(
@@ -78,3 +86,4 @@ module.exports = {
     }
   },
 };
+
